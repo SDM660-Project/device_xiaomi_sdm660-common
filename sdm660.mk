d5aa4847246f69873b65feb406b2464b074d1bd4
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    sdm660-common: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>

--- sdm660.mk
+++ sdm660.mk
@@ -404,6 +404,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator@1.2-service
