3fdfef06888f3a9d9b8acc4db3f932b610b64fa1
Jabiyeff <cebiyevanar@gmail.com>
2021-04-05 21:07:20 +0800

    sdm660-common: Import AdaptLaunch and some other perf * From LA.UM.8.6.2.r1-08600-89xx.0
    Test: Better app launching time
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Adapt Launch
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
