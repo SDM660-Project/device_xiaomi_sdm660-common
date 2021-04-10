5fabebb5248811f7d7c910c487fd40e5d3c06477
srfarias <erikofarias@gmail.com>
2021-04-10 01:37:12 +0000

    [SQUASH]sdm660-common: wfd bringup
    wfd blobs from phoenix_sprout:11/RKQ1.201123.002/210641533e0c8:user/release-keys
    
    Signed-off-by: srfarias <erikofarias@gmail.com>
    Change-Id: I48c860f93f530b94e01ebfdfb7d45a430c307896
    
    Revert "sdm660-common: Disable WiFi display for now"
    
    This reverts commit d411e3b057cacf92b3c378e750a03d2ebb28639b.
    
    sdm660-common: bump vendor.display.config to 2.0
    
    * New WFD blobs need vendor.display.confi@2.0.so
    
    sdm660-common: manifest: vendor.display.config 1.9
    
    * Some SGSI does not allow the use of vendor.display.config 2.0
    * As long as the vendor.display.confi@2.0.so file exists, WFD can still work normally.

--- sdm660.mk
+++ sdm660.mk
@@ -196,9 +196,11 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -212,7 +214,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -434,7 +437,8 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
-    libnl
+    libnl \
+    libstagefright_enc_common
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
