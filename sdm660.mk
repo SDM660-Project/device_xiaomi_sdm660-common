d5f85ab6ebe10f8d51fb1ef550658eefb67f00f8
Bhuvan Varshney <bvarshne@codeaurora.org>
2021-04-27 11:05:02 +0800

    sdm660-common: Uprev secure_element interface version
    Update the secure_element version to
    enable new upgraded interface solution
    
    Change-Id: Iac93719544ffe87db86ddf55ac7384bc555492e6
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -361,7 +361,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0 \
-    android.hardware.secure_element@1.1 \
+    android.hardware.secure_element@1.2 \
     rild \
     libavservices_minijail.vendor \
     librmnetctl \
