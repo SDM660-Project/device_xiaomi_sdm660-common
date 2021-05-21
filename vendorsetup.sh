7bbb181746062400c31335d3995f8b925daf14c6
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-05-21 19:00:39 -0300

    sdm660-common: Rework vendorsetup
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,18 @@
 # export
 export SKIP_ABI_CHECKS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 
 # rm -rf
+rm -rf hardware/qcom/wlan
+rm -rf hardware/qcom/audio
+rm -rf hardware/qcom/media
+rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
+git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
+git clone https://github.com/SDM660-Project/android_hardware_qcom_wlan hardware/qcom/wlan
 git clone https://github.com/ChrisW444/vendor_XiaomiParts -b 4.19-kernel vendor/XiaomiParts
 git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
 git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_data-ipa-cfg-mgr vendor/qcom/opensource/data-ipa-cfg-mgr
