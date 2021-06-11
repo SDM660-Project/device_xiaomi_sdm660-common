86ad9198a1e31a6220ab32f06b957004ffa60ecb
OdSazib <odsazib@gmail.com>
2021-06-12 00:29:52 +0600

    sdm660-common: Downgrade android.hardware.secure_element - Also remove manifest entry

--- sdm660.mk
+++ sdm660.mk
@@ -386,7 +386,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0 \
-    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.0 \
     rild \
     libavservices_minijail.vendor \
     librmnetctl \
